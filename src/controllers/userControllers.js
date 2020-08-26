b768769e57a021bb0a57c12be80db97197f8cdf7
Reuben Smith <reuben-smith@hotmail.com>
2020-08-26 09:59:08 +0930

    Adds handlers for registration
    * Modifies src/controllers/userControllers.js
        - Adds register function to register new user

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -5,10 +5,29 @@ import { UserSchema } from "../models/userModel";
 
 const User = mongoose.model("User", UserSchema);
 
+// Check a user is logged in
 export const loginRequired = (req, res, next) => {
     if (req.user) { // If we have a logged in user
         next(); // GET, POST, DELETE etc.
     } else {
         return res.status(401).json({ message: "Unauthorised user!" };)
     }
+};
+
+// Register a new user
+export const register = (req, res) => {
+    const newUser = new User(req.body);
+    // Encrypt the password before passing it to the database
+    newUser.hashPassword = bcrypt.hasSync(req.body.password, 10); // 10 = hashSync algorithm
+    // Now save the user to the database
+    newUser.save((err, user) => {
+        if (err) {
+            return res.status(400).send({
+                message: err
+            });
+        } else {
+            user.hashPassword = undefined; // We don't want to pass the password back, as this could be used for hacking
+            return res.json(user);
+        }
+    });
 };
